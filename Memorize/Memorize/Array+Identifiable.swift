a16e0553d1c8388c90e2fc8d48047bfe4b2ea9cc
Shan Weiqiang <schmessi@163.com>
2020-06-11 23:04:22 +0800

    end of first demo in lecture4

---
+++ Memorize/Memorize/Array+Identifiable.swift
@@ -0,0 +1,9 @@
+//
+//  Array+Identifiable.swift
+//  Memorize
+//
+//  Created by 单译 on 2020/6/11.
+//  Copyright © 2020 schmessi@163.com.shan.weiqiang. All rights reserved.
+//
+
+import Foundation
---
+++ Memorize/Memorize/Grid.swift
@@ -0,0 +1,21 @@
+//
+//  Grid.swift
+//  Memorize
+//
+//  Created by 单译 on 2020/6/10.
+//  Copyright © 2020 schmessi@163.com.shan.weiqiang. All rights reserved.
+//
+
+import SwiftUI
+
+struct Grid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Grid_Previews: PreviewProvider {
+    static var previews: some View {
+        Grid()
+    }
+}
